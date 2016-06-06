e03338380520566731f99f5c7485da05d1db7818
mwape97 <mwapepro@hotmail.com>
2016-06-06 13:27:41 +0200

    Draft_slider

--- app/src/main/java/com/a2id40group36/ayika/ayika/HomeActivity.java
+++ app/src/main/java/com/a2id40group36/ayika/ayika/HomeActivity.java
@@ -5,6 +5,8 @@ package com.a2id40group36.ayika.ayika;
  */
 
 import com.a2id40group36.ayika.ayika.R;
+
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,14 +14,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeActivity extends Fragment {
+ drawSlider draw;
+    Canvas canvas;
+    String number;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+//        draw.onDraw(canvas);
 
         return rootView;
+
     }
+
+
 
 }
---
+++ app/src/main/java/com/a2id40group36/ayika/ayika/sliderActivity.java
@@ -0,0 +1,11 @@
+package com.a2id40group36.ayika.ayika;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mwape on 05.06.2016.
+ */
+public class sliderActivity extends AppCompatActivity {
+
+
+}

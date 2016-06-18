63b313d49350fff8d17ada9036d54ddcec18414a
Stufingo <geniekort@hotmail.com>
2016-06-18 20:16:02 +0200

    Minor updates

--- app/src/main/java/com/a2id40group36/ayika/ayika/HomeActivity.java
+++ app/src/main/java/com/a2id40group36/ayika/ayika/HomeActivity.java
@@ -9,6 +9,7 @@ import com.a2id40group36.ayika.ayika.R;
 import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,16 +21,20 @@ import java.text.DateFormat;
 import java.util.Date;
 
 public class HomeActivity extends Fragment {
-public static TextView textView;
+    public static TextView textView;
+    public boolean m_iAmVisible;
     public static String currentDateTimeString = "";
+    View rootView;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
 
-
+        m_iAmVisible = false;
 
         return rootView;
 
@@ -41,6 +46,21 @@ public static TextView textView;
 
 
 
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        m_iAmVisible = isVisibleToUser;
+
+        Log.d("DEBUG", "setUserVisibleHint: " + m_iAmVisible);
+        if(getActivity() != null) {
+            ThrottleSlider x = ((ThrottleSlider) getActivity().findViewById(R.id.throttle));
+
+            if (!m_iAmVisible && x != null) {
+                Log.d("DEBUG", "setUserVisibleHint: ");
+                x.stoppedThread();
+            }
+        }
+    }
 
 
 }
--- app/src/main/java/com/a2id40group36/ayika/ayika/TemperatureChangeThread.java
+++ app/src/main/java/com/a2id40group36/ayika/ayika/TemperatureChangeThread.java
@@ -2,6 +2,8 @@ package com.a2id40group36.ayika.ayika;
 
 import android.app.Activity;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -13,8 +15,7 @@ public class TemperatureChangeThread implements Runnable {
 
     final ThrottleSlider t;
     final Activity a;
-    final TextView temptext;
-    public final float RATEOFTEMPCHANGE = (float)0.1; // In degrees per miliseconds
+    public final float RATEOFTEMPCHANGE = (float)0.023; // In degrees per 10 miliseconds
 
     public TemperatureChangeThread(ThrottleSlider to, Activity ac){
 
@@ -22,48 +23,27 @@ public class TemperatureChangeThread implements Runnable {
         this.t = to;
         a = ac;
 
-        temptext = (TextView) a.findViewById(R.id.temperatureText);
     }
 
 
     @Override
     public void run() {
-        int loop = 0;
-        Log.d("DEBUG", "In thread run" + t.currentChange);
-        while(t.currentChange != 0) {
+
+        while(t.currentChange != 0 && !t.stopThreadPlease) {
 
             t.addTemperature(t.currentChange * RATEOFTEMPCHANGE);
-            loop++;
 
-            Log.d("DEBUG", "In thread: " + t.currentChange);
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
                 Log.d("Error", "Could not sleep: " + e.getMessage());
             }
 
-
-            if(temptext != null) {
-                Log.d("DEBUG", "temptext is nonnull");
-                a.runOnUiThread(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        String nview = String.format("%.2f",t.sliderTemperature) + "°";
-                        temptext.setText(nview);
-                    }
-                });
-            }else{
-
-                Log.d("DEBUG", "temptext is null");
-            }
-
-
-
-
+            t.changeTempTextView();
 
         }
 
         t.stoppedThread();
+        t.stopThreadPlease = false;
     }
 }
